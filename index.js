a11bcab59055f7a974e1ac01c871b015622dca39
vishwanath <vishwanathholge18@gmail.com>
2022-04-24 15:07:25 +0530

    added javascripts

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function isValidate(){
     let firstName = document.getElementById("firstname").value;
-    let lastName = document.getElementById("firstname").value;
-    let email = document.getElementById("firstname").value;
-    let phone = document.getElementById("firstname").value;
-    let state = document.getElementById("firstname").value;
-    let zipcode = document.getElementById("firstname").value;
+    let lastName = document.getElementById("lastname").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phoneno").value;
+    let state = document.getElementById("state").value;
+    let zipcode = document.getElementById("zipcode").value;
+    let invalidCheck = document.getElementById("invalidCheck").value;
 
     if(firstName == ''){
         document.getElementById("firstNameInvalid").style.display = "block"
@@ -24,7 +25,7 @@ function isValidate(){
         document.getElementById("lastNameValid").style.display = "block";
         document.getElementById("lastNameInvalid").style.display = "none";
     }
-    if(email == '' || !email.includes('@') || email.startswith('@')){
+    if(email == '' || !email.includes('@') || !email.includes('.') ||email.startsWith('@') || email.indexOf("@")> email.indexOf(".")){
         document.getElementById("emailInvalid").style.display = "block"
         document.getElementById("emailValid").style.display = "none"
 
@@ -33,13 +34,37 @@ function isValidate(){
         document.getElementById("emailValid").style.display = "block";
         document.getElementById("emailInvalid").style.display = "none";
     }
-    if(email == ''){
-        document.getElementById("emailInvalid").style.display = "block"
-        document.getElementById("emailValid").style.display = "none"
+    if(phone  == ''|| phone.length < 10 || phone.length >10){
+        document.getElementById("phoneInvalid").style.display = "block"
+        document.getElementById("phoneValid").style.display = "none"
 
     }
     else{
-        document.getElementById("emailValid").style.display = "block";
-        document.getElementById("emailInvalid").style.display = "none";
+        document.getElementById("phoneValid").style.display = "block";
+        document.getElementById("phoneInvalid").style.display = "none";
     }
+    if(state == ''){
+        document.getElementById("stateInvalid").style.display = "block"
+        document.getElementById("stateValid").style.display = "none"
+
+    }
+    else{
+        document.getElementById("stateValid").style.display = "block";
+        document.getElementById("stateInvalid").style.display = "none";
+    }
+    if(zipcode  == ''|| zipcode.length < 6 ){
+        document.getElementById("zipInvalid").style.display = "block"
+        document.getElementById("zipValid").style.display = "none"
+
+    }
+    else{
+        document.getElementById("zipValid").style.display = "block";
+        document.getElementById("zipInvalid").style.display = "none";
+    }
+    if(  invalidCheck == ''  ){
+        document.getElementsByClassName("Invalid-feedback").style.display = "block"
+        
+
+    }
+    
 }
